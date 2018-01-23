0903101d1b7e4dd90383185a88402febb25ecfeb
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-23 22:42:14 +1100

    Initial commit

---
+++ multiplication_of_two_binary_numbers/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * C# Program to Find Multiplication of two Binary Numbers
+ */
+
+using System;
+
+namespace multiplication_of_two_binary_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public static void Main()
+            {
+
+                int binary1, binary2, multiply = 0;
+                int digit, factor = 1;
+                prog pg = new prog();
+                Console.WriteLine("Enter the first binary number: ");
+                binary1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the second binary number: ");
+                binary2 = int.Parse(Console.ReadLine());
+                while (binary2 != 0)
+                {
+                    digit = binary2 % 10;
+                    if (digit == 1)
+                    {
+                        binary1 = binary1 * factor;
+                        multiply = pg.binaryproduct(binary1, multiply);
+                    }
+                    else
+                    {
+                        binary1 = binary1 * factor;
+                        binary2 = binary2 / 10;
+                        factor = 10;
+                    }
+                    Console.WriteLine("Product of two binary numbers: {0}", multiply);
+                    Console.ReadLine();
+                }
+            }
+
+
+        }
+    }
+}
